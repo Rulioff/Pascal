66b4c2da427bd31b40c51ecc125c714fe8aab1de
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-04-20 21:00:29 +0300

    Create P_10_1.pas
    The program checks the password.

---
+++ P_10_1.pas
@@ -0,0 +1,11 @@
+﻿// Программа проверяет пароль.
+var
+  Password_Etalon, Paswword_Ask : string;
+  begin
+    Password_Etalon := 'pascal';
+    Writeln('Введите пожалуйста пароль:  '); Readln(Paswword_Ask);
+    if Password_Etalon = Paswword_Ask
+      then Writeln('Проходите пожалуйста.')
+      else Writeln('Доступ запрещён.');
+      Writeln('Для завершения нажмите пожалуйста Enter.');  Readln;
+  end.
