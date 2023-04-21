098eb75610f73c3acf155632e79cbcd7210a983a
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-04-21 20:43:33 +0300

    Create P_10_2.pas
    Неполный условный оператор.

---
+++ P_10_2.pas
@@ -0,0 +1,12 @@
+﻿// Программа проверяет пароль.
+var
+  Password_Etalon, Paswword, Answer : string;
+  begin
+    Password_Etalon := 'pascal';
+    Writeln('Введите пожалуйста пароль:  '); Readln(Paswword);
+    Answer := 'Доступ запрещён!';
+    if Password_Etalon = Paswword
+      then Answer := 'Проходите пожалйста';
+      Writeln(Answer);
+      Writeln('Для завершения программы нажмите пожалуйста Enter');  Readln;
+  end.
