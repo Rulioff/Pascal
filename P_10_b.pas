d3cce9b99d0b5a700bcf3f6b4279c09b8ed15cd4
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-04-25 19:36:50 +0300

    Create P_10_b.pas
    Неполный цикл if.

---
+++ P_10_b.pas
@@ -0,0 +1,10 @@
+﻿// Программа "Метеоцентр"
+var Rain, Weather : string;
+begin
+  Rain := 'Нет';
+  Writeln('Сейчас идёт дождь?');
+  Readln(Rain);
+  if Rain = 'Да'
+    then Weather := 'А зонта-то у тебя нет!';
+  Writeln(Weather); Readln;
+  end.
