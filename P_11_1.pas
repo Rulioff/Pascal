db7489d2c12faf8f5aa5667e8a3ea3eb8f94323a
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-04-25 20:27:12 +0300

    Create P_11_1.pas
    Операторные блоки.

---
+++ P_11_1.pas
@@ -0,0 +1,15 @@
+﻿// Операторные блоки.
+
+var Password : string;
+begin
+  Writeln('Введите пожалуйста пароль'); Readln(Password);
+  if Password = 'pascal' then begin
+    Writeln('Открыть ворота!');
+    Writeln('Оркестр, тушь!');
+    Writeln('Проходите пожалуйста')
+  end     else begin
+          Writeln('Вы ввели неверный пароль.');
+          Writeln('Оставайтесь на месте до выяснения.')
+  end;
+  Writeln('Нажмите Enter'); Readln;
+end.
