07b09eba83d09922669e18c87b321f7620999b2b
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-04-28 21:52:09 +0300

    Create P_12_3.pas
    часовой с досрочным выходом из цикла.

---
+++ P_12_3.pas
@@ -0,0 +1,12 @@
+﻿{ P_12_3 – часовой с досрочным выходом из цикла }
+var Password : string;
+begin
+    repeat
+      Writeln('Введите пожалуйста пароль:  '); Readln(Password);
+      if Password = '' then Break;
+          if Password = 'pascal'
+               then Writeln('Проходите пожалуйста.')
+               else Writeln('Доступ запрещён.');
+    until Password = ''; {Окончание цикла}
+Writeln('Для завершения нажмите пожалуйста Enter.');  Readln;
+end.
