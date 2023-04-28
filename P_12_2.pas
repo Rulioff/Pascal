64c809fe04a8e17cc1f3ee82711c56a4e4b3f84a
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-04-28 21:52:36 +0300

    Create P_12_2.pas
    P_12_2 Программа работает в цикле при 2-х условиях.

---
+++ P_12_2.pas
@@ -0,0 +1,12 @@
+﻿{P_12_2 Программа работает в цикле при 2-х условиях.}
+var Password : string;
+begin
+    repeat
+      Writeln('Введите пожалуйста пароль:  '); Readln(Password);
+      if Password <> '' then
+          if Password = 'pascal'
+               then Writeln('Проходите пожалуйста.')
+               else Writeln('Доступ запрещён.');
+    until Password = ''; {Окончание цикла}
+Writeln('Для завершения нажмите пожалуйста Enter.');  Readln;
+end.
