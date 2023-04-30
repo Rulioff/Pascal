d6374de2ad00578a28d3170d00258f7d49339e59
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-04-30 18:49:47 +0300

    Create P_13_1.pas
    P_13_1 Булевы переменные.

---
+++ P_13_1.pas
@@ -0,0 +1,11 @@
+﻿{P_13_1 Первый маршрут проезда.}
+var
+  Waypoint_A, Waypoint_B : boolean;  Open : string;
+begin
+  Write('Введите 1, если улица А открыта ');  Readln(Open);   Waypoint_A := Open = '1';
+  Write('Введите 1, если улица B открыта ');  Readln(Open);   Waypoint_B := Open = '1';
+  Open := 'Можно идти.';
+  if Waypoint_A and Waypoint_B then Open := 'Можно ехать.';
+     Writeln(Open);
+     Writeln('Для выхода нажмите пожалуйста Enter');   Readln;
+end.
