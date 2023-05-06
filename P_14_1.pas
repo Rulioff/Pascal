94ff9611770273502f306a741a18c996aa6461ae
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-06 20:31:54 +0300

    Update P_14_1.pas
    Заменил переменную Result на res.

--- P_14_1.pas
+++ P_14_1.pas
@@ -1,13 +1,14 @@
 ﻿{P_14_1 Экзаменатор базовый.}
 var
-  Multiplier_1, Multiplier_2 : integer; Result : Boolean; Answer : string;
+  multiplier_1, multiplier_2, res : integer; compare : Boolean; answer : string;
 begin
-  Repeat
-    Write('Введите 1-й множитель:  ');  Read(Multiplier_1);
-    Write('Введите 2-й множитель:  ');  Read(Multiplier_2);
-    Write('Введите произведение:  ');  Read(Result);
-    if Result = 0 then Break;
-     if Result = Multiplier_1 * Multiplier_2 then Result := ('Верно!')
+  repeat
+    write('Введите 1-й множитель:  ');  read(Multiplier_1);
+    write('Введите 2-й множитель:  ');  read(Multiplier_2);
+    write('Введите произведение:  ');  read(Result);
+    if res = 0 then Break; {Конец цикла при 0.}
+    {Проверяем ответ.}
+     if res = Multiplier_1 * Multiplier_2 then Result := ('Верно!')
      else Result := ('Повторите таблицу пожалуйста.');
      Writeln(Result); Readln();
   until Result = 0;
