8d72fdacc28b840f5f69c16b6a45a6cdb53d8c65
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-06 15:05:27 +0300

    Create P_14_1.pas
    P_14_1 Экзаменатор базовый.

---
+++ P_14_1.pas
@@ -0,0 +1,14 @@
+﻿{P_14_1 Экзаменатор базовый.}
+var
+  Multiplier_1, Multiplier_2 : integer; Result : Boolean; Answer : string;
+begin
+  Repeat
+    Write('Введите 1-й множитель:  ');  Read(Multiplier_1);
+    Write('Введите 2-й множитель:  ');  Read(Multiplier_2);
+    Write('Введите произведение:  ');  Read(Result);
+    if Result = 0 then Break;
+     if Result = Multiplier_1 * Multiplier_2 then Result := ('Верно!')
+     else Result := ('Повторите таблицу пожалуйста.');
+     Writeln(Result); Readln();
+  until Result = 0;
+end.
