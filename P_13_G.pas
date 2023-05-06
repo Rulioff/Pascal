7c3762f602cfdf0ad157eec5d31a859bd7ba41c3
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-06 20:30:21 +0300

    Update P_13_G.pas
    Добавил переменную win.

--- P_13_G.pas
+++ P_13_G.pas
@@ -1,24 +1,24 @@
 ﻿{P_13_G Орлянка.}
 var
-  Coin_1, Coin_2, Coin_3 : boolean; Toss : string;
+  coin_1, coin_2, coin_3 : boolean; toss, win : string;
 begin
-  Write('Введите 1 если на первой монете выпал орёл '); Readln(Toss);   Coin_1 := Toss = '1';
-  Write('Введите 2 если на первой монете выпал орёл '); Readln(Toss);   Coin_2 := Toss = '1';
-  Write('Введите 3 если на первой монете выпал орёл '); Readln(Toss);   Coin_3 := Toss = '1';
+  Write('Введите 1 если на первой монете выпал орёл '); Readln(toss);   coin_1 := toss = '1';
+  Write('Введите 2 если на первой монете выпал орёл '); Readln(toss);   coin_2 := toss = '1';
+  Write('Введите 3 если на первой монете выпал орёл '); Readln(toss);   coin_3 := toss = '1';
 
-  Toss  := 'Выигрывает казино.';
-  if Coin_1 and Coin_2 and Coin_3
-     then Toss := 'Выиграл игрок 1'
-  else if not Coin_1 and not Coin_2 and not Coin_3
-     then Toss := 'Выиграл игрок 2'
-  else if not (Coin_1 = Coin_2) and (Coin_2 = Coin_3)
-     then Toss := 'Выиграл игрок 3'
-  else if not Coin_1 and Coin_2 and Coin_3
-     then Toss := 'Выиграл игрок 4'
-  else if Coin_1 and (Coin_2 = Coin_3)
-     then Toss := 'Выиграл игрок 5'
-  else Toss := 'Казино закрывается';
+  toss  := 'Выигрывает казино.';
+  if coin_1 and coin_2 and coin_3
+     then win := 'Выиграл игрок 1';     Writeln(win);
+  if not coin_1 and not coin_2 and not coin_3
+     then win := 'Выиграл игрок 2';     Writeln(win);
+  if (coin_1 = coin_2 = coin_3) or (not coin_1 and not coin_2 and not coin_3)
+     then win := 'Выиграл игрок 3';     Writeln(win);
+  if not coin_1 and coin_2 and coin_3
+     then win := 'Выиграл игрок 4';     Writeln(win);
+  if coin_1 and (coin_2 = coin_3)
+     then win := 'Выиграл игрок 5';     Writeln(win);
+//  else win := 'Казино закрывается';
 
-     Writeln(Toss);
+//     Writeln(win);
      Writeln('Для выхода нажмите пожалуйста Enter');   Readln;
 end.
