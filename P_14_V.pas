a023f4f7ac2cead3a125cc0580af3a1da5d0d465
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-07 17:41:31 +0300

    Create P_14_V.pas
    P_14_V Определение наибольшего из 3-х чисел.

---
+++ P_14_V.pas
@@ -0,0 +1,18 @@
+﻿{P_14_V Определение наибольшего из 3-х чисел.}
+var number_a, number_b, number_c, max : integer;
+     compare : Boolean;
+begin
+  repeat
+     write('Введите число A    ');  readln(number_a);
+     write('Введите число B    ');  readln(number_b);
+     write('Введите число C    ');  readln(number_c);
+     compare := (number_a > number_b) and (number_b > number_c);  {compare = true, если number_a наибольшее число.}
+     if compare
+          then max := number_a;
+     if (number_b > number_a) and (number_b > number_c)
+          then max := number_b
+     else max := number_c;
+     writeln('Наибольшим является  ', max);
+     writeln('Для выхода из программы введите все нули.');
+  until max = 0; 
+end.
