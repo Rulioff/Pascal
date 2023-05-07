8a0fb03b2f5df14a6ed3e035e5852820cd8b0fd7
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-07 14:23:31 +0300

    Update P_14_1.pas
    P_14_1 Экзаменатор базовый.

--- P_14_1.pas
+++ P_14_1.pas
@@ -1,15 +1,20 @@
 ﻿{P_14_1 Экзаменатор базовый.}
 var
-  multiplier_1, multiplier_2, res : integer; compare : Boolean; answer : string;
+  multiplier_1, multiplier_2, res : integer; {Множители и произведение.}
+  compare : Boolean;     {Логическое сравнение.}
+  answer : string;  {Вывод оценки.}
 begin
   repeat
-    write('Введите 1-й множитель:  ');  read(Multiplier_1);
-    write('Введите 2-й множитель:  ');  read(Multiplier_2);
-    write('Введите произведение:  ');  read(Result);
-    if res = 0 then Break; {Конец цикла при 0.}
+    write('Введите 1-й множитель:  ');  readln(multiplier_1);
+    write('Введите 2-й множитель:  ');  readln(multiplier_2);
+    write('Введите произведение:  ');  readln(res);
+    if res = 0 then break; {Конец цикла при 0.}
     {Проверяем ответ.}
-     if res = Multiplier_1 * Multiplier_2 then Result := ('Верно!')
-     else Result := ('Повторите таблицу пожалуйста.');
-     Writeln(Result); Readln();
-  until Result = 0;
+     compare := multiplier_1 * multiplier_2 = res;   {compare = true, если ответ верный.}
+     if compare
+          then answer := 'Верно!'
+     else answer := 'Повторите таблицу пожалуйста.';
+     writeln(answer);
+     writeln('Для завершения экзамена введите 0 пожалуйста.');   readln();
+  until res = 0;    {Бесконечный цикл.}
 end.
