cf505341c51f9b39beddd027d4aa289867631645
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-14 20:24:53 +0300

    Update P_14_G.pas
    правка

--- P_14_G.pas
+++ P_14_G.pas
@@ -1,22 +1,22 @@
 ﻿{P_14_V Кирпич проходит?}
 var side_a, side_b, brick_x, brick_y, brick_z : integer;
-     compareHole, compareBrick, compareSize : Boolean;
+     isHoleError, compareBrick, compareSize : Boolean;
 begin
   repeat    
      write('Введите ширину отверстия    ');  readln(side_a);
      write('Введите высоту отверстия    ');  readln(side_b);
-     compareHole := side_a > side_b;  {compare = true, если ширина больше высоты, выходим из цикла.}
-     if compareHole     then break;
+//     isHoleError := side_a > side_b;  {compare = true, если ширина больше высоты, выходим из цикла.}
+     if side_a > side_b     then break;
      write('Введите ширину кирпича '); readln(brick_x);
      write('Введите высоту кирпича '); readln(brick_y);
      write('Введите длину кирпича '); readln(brick_z);
-     compareBrick := (brick_x > brick_y) or (brick_y > brick_z);  {compare = true, если ширина больше высоты, выходим из цикла.}
-     if compareBrick     then break;
-     compareSize := (side_a > brick_x) and (side_b > brick_y);  {compare = true, если отверстие больше кирпича.}
-     if compareSize
+//     compareBrick := (brick_x > brick_y) or (brick_y > brick_z);  {compare = true, если ширина больше высоты, выходим из цикла.}
+     if (brick_x > brick_y) or (brick_y > brick_z)     then break;
+//     compareSize := (side_a >= brick_x) and (side_b >= brick_y);  {compare = true, если отверстие больше кирпича.}
+     if (side_a >= brick_x) and (side_b >= brick_y)
           then writeln('Кирпич провалился')
      else writeln('Кирпич застрял.');
-  until compareHole or compareBrick;
+  until true;
 writeln('Ширина должна быть меньше высоты и меньше длины. Поторите ввод.');
 writeln('Для завершения нажмите Enter пожалуйста.');   readln();
 end.
