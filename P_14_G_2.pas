40c6bcb491285b60a98933f98ca2533186b9a31a
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-14 14:54:31 +0300

    Update P_14_G_2.pas
    Неверное решение.

--- P_14_G_2.pas
+++ P_14_G_2.pas
@@ -1,25 +1,31 @@
 ﻿{P_14_V Определить ширину.}
-var side_a, side_b, brick_x, brick_y, brick_z : integer;
-     compareHole, compareBrick, compareSize : Boolean;
+var side1, side2, side_a {ширина}, side_b {высота}, brick1, brick2, brick3, brick_x, brick_y, brick_z : integer;
+     compareHole, compareWidth, compareHight, compareLength, compareSize : Boolean;
 begin
   repeat    
-     write('Введите ширину отверстия    ');  readln(side_a);
-     write('Введите высоту отверстия    ');  readln(side_b);
-     compareHole := side_a < side_b;  {compare = true, если side_a меньше, то это ширина.}
+     write('Введите 1-ю сторону отверстия    ');  readln(side1);
+     write('Введите 2-ю сторону отверстия    ');  readln(side2);
+     compareHole := side1 < side2;  {compare = true, если side_a меньше, то это ширина.}
      if compareHole
-          then writeln('Ширина = ', (side_a)); writeln('Высота = ', (side_b));
+          then side_a := side1; side_b := side2;
      if compareHole = false
-          then writeln('Ширина = ', (side_b)); writeln('Высота = ', (side_a));
-     write('Введите ширину кирпича '); readln(brick_x);
-     write('Введите высоту кирпича '); readln(brick_y);
-     write('Введите длину кирпича '); readln(brick_z);
-     compareBrick := (brick_x > brick_y) or (brick_y > brick_z);  {compare = true, если ширина больше высоты, выходим из цикла.}
-     if compareBrick     then break;
+          then side_a := side2; side_b := side1;
+     writeln('Ширина = ', (side_a), ',   Высота = ', (side_b));
+     
+     write('Введите 1-ю сторону кирпича '); readln(brick1);
+     write('Введите 2-ю сторону кирпича '); readln(brick2);
+     write('Введите 3-ю сторону кирпича '); readln(brick3);
+     compareWidth := (brick1 < brick2) and (brick1 < brick3);  {compare = true, если ширина меньше высоты, ширина side1.}
+     if compareWidth
+          then brick_x := brick1;
+       if compareWidth = false
+          then brick_x := brick3; brick_y := brick2; brick_z := brick1;
+     writeln('Ширина = ', (brick_x), ',   Высота = ', (brick_y), ',   Длина = ', (brick_z));
      compareSize := (side_a > brick_x) and (side_b > brick_y);  {compare = true, если отверстие больше кирпича.}
      if compareSize
           then writeln('Кирпич провалился')
           else writeln('Кирпич застрял.');
-  until compareHole or compareBrick;
-writeln('Ширина должна быть меньше высоты и меньше длины. Поторите ввод.');
+  until not compareSize;
+writeln('Ширина должна быть меньше высоты и меньше длины. Повторите ввод.');
 writeln('Для завершения нажмите Enter пожалуйста.');   readln();
 end.
