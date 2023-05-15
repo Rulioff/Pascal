09315f2d35809cee879fd4f6f2a48cfb7c1ff736
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-15 20:30:32 +0300

    Update P_14_G.pas
    Приглаживание кода.

--- P_14_G.pas
+++ P_14_G.pas
@@ -3,20 +3,28 @@ var sideA, sideB, brickX, brickY, brickZ : integer;
 
 begin
   repeat    
-     write('Введите ширину отверстия: ');  readln(sideA);
-     write('Введите высоту отверстия: ');  readln(sideB);
-//     isHoleError := side_a > side_b;  {compare = true, если ширина больше высоты, выходим из цикла.}
-     if sideA > sideB
+     write('Введите ширину отверстия: ');
+     readln(sideA);
+     write('Введите высоту отверстия: ');
+     readln(sideB);
+
+     if sideA > sideB //Если ширина больше высоты, выходим из цикла.
       then break;
-      write('Введите ширину кирпича: '); readln(brickX);
-      write('Введите высоту кирпича: '); readln(brickY);
-      write('Введите длину кирпича: '); readln(brickZ);
-//     compareBrick := (brick_x > brick_y) or (brick_y > brick_z);  {compare = true, если ширина больше высоты, выходим из цикла.}
-     if (brickX > brickY) or (brickY > brickZ)     then break;
-//     compareSize := (side_a >= brick_x) and (side_b >= brick_y);  {compare = true, если отверстие больше кирпича.}
-     if (sideA >= brickX) and (sideB >= brickY)
+      
+      write('Введите ширину кирпича: ');
+      readln(brickX);
+      write('Введите высоту кирпича: ');
+      readln(brickY);
+      write('Введите длину кирпича: ');
+      readln(brickZ);
+
+     if (brickX > brickY) or (brickY > brickZ)  //Если ширина больше высоты, выходим из цикла.
+      then break;
+
+     if (sideA >= brickX) and (sideB >= brickY) // Сравниваем отверстие и кирпич.
       then writeln('Кирпич провалился.')
      else writeln('Кирпич застрял.');
   until true;
+  
 writeln('Ширина должна быть меньше высоты и меньше длины.');
 end.
