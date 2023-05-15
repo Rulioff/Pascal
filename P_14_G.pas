f4dcd21c06ff4cac72067fef4f54d87771dcd5b5
Алексей Рулёв <ruljova@gmail.com>
2023-05-15 12:53:41 +0300

    Update P_14_G.pas
    Оформление под camelCaps.

--- P_14_G.pas
+++ P_14_G.pas
@@ -1,22 +1,22 @@
-﻿{P_14_V Кирпич проходит?}
-var side_a, side_b, brick_x, brick_y, brick_z : integer;
-     isHoleError, compareBrick, compareSize : Boolean;
+﻿{P_14_G Кирпич проходит?}
+var sideA, sideB, brickX, brickY, brickZ : integer;
+
 begin
   repeat    
-     write('Введите ширину отверстия    ');  readln(side_a);
-     write('Введите высоту отверстия    ');  readln(side_b);
+     write('Введите ширину отверстия: ');  readln(sideA);
+     write('Введите высоту отверстия: ');  readln(sideB);
 //     isHoleError := side_a > side_b;  {compare = true, если ширина больше высоты, выходим из цикла.}
-     if side_a > side_b     then break;
-     write('Введите ширину кирпича '); readln(brick_x);
-     write('Введите высоту кирпича '); readln(brick_y);
-     write('Введите длину кирпича '); readln(brick_z);
+     if sideA > sideB
+      then break;
+      write('Введите ширину кирпича: '); readln(brickX);
+      write('Введите высоту кирпича: '); readln(brickY);
+      write('Введите длину кирпича: '); readln(brickZ);
 //     compareBrick := (brick_x > brick_y) or (brick_y > brick_z);  {compare = true, если ширина больше высоты, выходим из цикла.}
-     if (brick_x > brick_y) or (brick_y > brick_z)     then break;
+     if (brickX > brickY) or (brickY > brickZ)     then break;
 //     compareSize := (side_a >= brick_x) and (side_b >= brick_y);  {compare = true, если отверстие больше кирпича.}
-     if (side_a >= brick_x) and (side_b >= brick_y)
-          then writeln('Кирпич провалился')
+     if (sideA >= brickX) and (sideB >= brickY)
+      then writeln('Кирпич провалился.')
      else writeln('Кирпич застрял.');
   until true;
-writeln('Ширина должна быть меньше высоты и меньше длины. Поторите ввод.');
-writeln('Для завершения нажмите Enter пожалуйста.');   readln();
+writeln('Ширина должна быть меньше высоты и меньше длины.');
 end.
