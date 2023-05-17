2dbad2e5a025663ef78a37cbccbb4907ddccfca7
Алексей Рулёв <ruljova@gmail.com>
2023-05-17 12:52:53 +0300

    Create P_15_2.pas
    P_15_2 -Пятикратный вызов функции Random (100) после Randomize.

---
+++ P_15_2.pas
@@ -0,0 +1,9 @@
+﻿{P_15_2 -Пятикратный вызов функции Random (100) после Randomize.}
+begin
+  randomize;
+  writeln(random(100));
+  writeln(random(100));
+  writeln(random(100));
+  writeln(random(100));
+  writeln(random(100));
+end.
