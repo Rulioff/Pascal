024afbc54127c36f0b3dfdbb975131182ddd73fd
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-17 20:49:26 +0300

    Create P_15_A.pas
    P_15_A В каких пределах будут генерироваться числа?

---
+++ P_15_A.pas
@@ -0,0 +1,7 @@
+﻿{P_15_A В каких пределах будут генерироваться числа?}
+begin
+  randomize; // <--
+  writeln(10 + random(10));
+  writeln(random(10) + random(10));
+  writeln(random(5) + random(5) + random(5) + random(5));
+end.
