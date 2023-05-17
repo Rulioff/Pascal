f4055ca6b6abbe4a26903ae2e42a82e4ddcd98c2
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-17 20:38:31 +0300

    Update P_15_2.pas
    Псевдослучайная функция random.

--- P_15_2.pas
+++ P_15_2.pas
@@ -1,6 +1,6 @@
 ﻿{P_15_2 -Пятикратный вызов функции Random (100) после Randomize.}
 begin
-  randomize;
+  randomize(5); // <--
   writeln(random(100));
   writeln(random(100));
   writeln(random(100));
