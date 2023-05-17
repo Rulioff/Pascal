cd11f6d34ddb02cb4d2ec75a70ca4d28e359f305
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-17 21:29:16 +0300

    Update P_15_A.pas
    Ответы.

--- P_15_A.pas
+++ P_15_A.pas
@@ -1,7 +1,7 @@
 ﻿{P_15_A В каких пределах будут генерироваться числа?}
 begin
   randomize; // <--
-  writeln(10 + random(10));
-  writeln(random(10) + random(10));
-  writeln(random(5) + random(5) + random(5) + random(5));
+  writeln(10 + random(10)); //10...19
+  writeln(random(10) + random(10)); //0...19
+  writeln(random(5) + random(5) + random(5) + random(5)); //0...19
 end.
