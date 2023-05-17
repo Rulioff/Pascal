e83d8e96dbf8753347810f09c60de65dcf28519a
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-17 21:24:15 +0300

    Create P_15_G.pas
    P_15_G Ряд случайных булевых значений (False, True)

---
+++ P_15_G.pas
@@ -0,0 +1,10 @@
+﻿{P_15_G Ряд случайных булевых значений (False, True)}
+var i : integer;
+begin
+  randomize; // <--
+  i := 0;
+  repeat
+    writeln(random(2) = random(2));
+    i := i + 1;
+  until i = 20;
+end.
