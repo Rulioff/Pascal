91f637ae2e08b2dad8a806cb7f390cd5dea52cd1
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-18 21:40:36 +0300

    Create Z_1.1.pas
    Z_1.1 Стоимость учебников.

---
+++ Z_1.1.pas
@@ -0,0 +1,16 @@
+﻿{Z_1.1 Стоимость учебников.}
+var rub, kop, quantity, price : integer;
+
+begin
+  write('Введите рубли за 1 учебник:');
+  readln(rub);
+  write('Введите копейки за 1 учебник:');
+  readln(kop);
+  write('Введите количество учебников:');
+  readln(quantity);
+  if kop > 100 then begin
+    rub := rub + 1;
+    kop := kop - 100;
+  end;
+  writeln('На ', quantity, ' учебников потрачено: ', rub * quantity, 'рублей и ', kop * quantity, ' копеек.');
+end.
