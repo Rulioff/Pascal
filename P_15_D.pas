f47a0a48d261ab57aa2e725e2a1839c5e1290108
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-18 20:51:56 +0300

    Create P_15_D.pas
    P_15_D Сгенерируйте случайные числа так, чтобы они не совпадали.

---
+++ P_15_D.pas
@@ -0,0 +1,15 @@
+﻿{P_15_D Сгенерируйте случайные числа так, чтобы они не совпадали.}
+var a, b, c : integer;
+begin
+  randomize;
+  repeat
+    a := 1 + random(10);
+    b := 1 + random(10);
+    c := 1 + random(10);
+    if (a <> b) and (a <> c) and (b <> c) //Проверяем, что числа не совпадают.
+      then
+    writeln('a = ', (a));
+    writeln('b = ', (b));
+    writeln('c = ', (c));
+  until false; //Бесконечный цикл.
+end.
