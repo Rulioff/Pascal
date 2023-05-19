c6d14a56ecb52de269f84909ca4806227bfe1dd0
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-19 20:56:45 +0300

    Create Z_1.2.pas
    Z_1.2 Одинаковые цифры.

---
+++ Z_1.2.pas
@@ -0,0 +1,19 @@
+﻿{Z_1.2 Одинаковые цифры.}
+var n : array of integer;
+  i : integer;
+  bool : boolean;
+  
+begin  
+  repeat
+    writeln('Введите двузначное число с одинаковым значением разряда: ');
+    readln(i);
+    if i<>0 then begin
+      SetLength(n, Length(n) +1 );
+      n[n.Length - 1] := trunc(Log10(abs(i))) + 1;
+    end;
+   until i= 0;
+    if (n[0]) = (n[1])
+      then bool := true;
+    for i := 0 to n.Length - 1 do writeln(n[i], ' ', (bool));
+
+end.
