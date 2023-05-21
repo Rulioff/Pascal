8c923cf86849894019da5ae4782c16bdfd7f5363
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-21 21:23:35 +0300

    Create Z_1.4.pas
    Z_1.4 Количество цифр в числе.

---
+++ Z_1.4.pas
@@ -0,0 +1,20 @@
+﻿{Z_1.4 Количество цифр в числе.}
+var summand, addend, amount, number : integer;
+  dig : byte;
+  
+begin  
+    write('Введите первое слагаемое: ');
+    readln(summand);
+    write('Введите второе слагаемое: ');
+    readln(addend);
+    dig := 0;
+    amount := summand + addend;
+    number := amount;
+    while number > 0 do begin
+      number := number div 10;
+      inc(dig);
+    end;
+    //writeln(number);
+
+    writeln('Сумма чисел: ', summand, ' и ', addend, ' содержит ', dig, ' цифр.');
+end.
