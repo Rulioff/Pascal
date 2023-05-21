7b6c24fe4f3250142c04fa8b8a023edb33c4a230
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-21 18:57:15 +0300

    Create Z_1.3.pas
    Z_1.3 Чётность цифр.

---
+++ Z_1.3.pas
@@ -0,0 +1,23 @@
+﻿{Z_1.3 Чётность цифр.}
+var number, count : integer;
+  even, odd : byte;
+  bool : boolean;
+  
+begin  
+    writeln('Введите число, которе содержит только чётные цифры: ');
+    readln(number);
+    odd := 0;
+    count := number;
+    while number > 0 do begin
+      if number mod 2 = 0
+        then even := even + 1
+      else
+        odd := odd +1;       
+      number := number div 10;
+    end;
+    writeln('Чётных ', even);
+    writeln('Нечётных ', odd);
+    if odd = 0
+      then bool := true;
+    writeln(count, ' содержит только чётные числа. ', (bool));
+end.
