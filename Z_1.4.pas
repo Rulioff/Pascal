c5bb594da31d5889f638a909f873e212da9ac433
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-21 21:52:07 +0300

    Update Z_1.4.pas
    Z_1.4_Правка.

--- Z_1.4.pas
+++ Z_1.4.pas
@@ -1,20 +1,21 @@
 ﻿{Z_1.4 Количество цифр в числе.}
-var summand, addend, amount, number : integer;
+var summand, addend, amount : integer;
   dig : byte;
   
 begin  
     write('Введите первое слагаемое: ');
     readln(summand);
+    
     write('Введите второе слагаемое: ');
     readln(addend);
+    
     dig := 0;
     amount := summand + addend;
-    number := amount;
-    while number > 0 do begin
-      number := number div 10;
+
+    while amount > 0 do begin
+      amount := amount div 10;
       inc(dig);
     end;
-    //writeln(number);
 
     writeln('Сумма чисел: ', summand, ' и ', addend, ' содержит ', dig, ' цифр.');
 end.
