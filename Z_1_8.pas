afe0eabd6d20f084c53e25b478418ab6c9dbb94f
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-24 20:35:40 +0300

    Create Z_1_8.pas
    Z_1_8 Логические выражения для чисел.

---
+++ Z_1_8.pas
@@ -0,0 +1,15 @@
+﻿{Z_1_8 Логические выражения для чисел}
+var first, second, third : integer;
+  equally, different, two, odd : boolean;
+begin
+  write('Введите положительное целое число: ');
+  readln(first);
+  
+  write('Введите положительное целое число: ');
+  readln(second);
+  
+  write('Введите положительное целое число: ');
+  readln(third);
+  
+  
+end.
