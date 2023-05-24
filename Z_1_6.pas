df05b2bf0d525cbc7ed1bc8179923324e1193923
Алексей Рулёв <ruljova@gmail.com>
2023-05-24 14:41:30 +0300

    Create Z_1_6.pas
    Z_1_6 Размещение палаток (2)

---
+++ Z_1_6.pas
@@ -0,0 +1,35 @@
+﻿{Z_1_6 Размещение палаток (2)}
+var campingN, campingM, tentK, tentL, tentQ, buffer : integer;
+begin
+  repeat
+    write('Введите ширину кемпинга в метрах: ');
+    readln(campingN);
+  
+    write('Введите длину кемпинга в метрах: ');
+    readln(campingM);
+    
+    if campingN > campingM then begin //Проверка длины и ширины.
+       buffer := campingN;
+       campingN := campingM;
+       campingM := buffer;
+    end;
+  
+    write('Введите 1-ю сторону палатки в метрах: ');
+    readln(tentK);
+    
+    write('Введите 2-ю сторону палатки в метрах: ');
+    readln(tentL);
+    
+    if tentK < tentL then begin //Проверка длины и ширины.
+       buffer := tentK;
+       tentK := tentL;
+       tentL := buffer;
+    end;
+       
+    tentQ := (campingN div tentK) * (campingM div tentL);
+    
+  until true;
+  
+  writeln('Кемпинг шириной ', campingN, ' и длиной ', campingM, ' метров вмещает ', tentQ, ' палток.')
+  
+end.
