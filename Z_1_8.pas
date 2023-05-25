fe129a02ab6d1d3a0376126713e8190e914d0761
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-25 21:36:18 +0300

    Update Z_1_8.pas
    Z_1_8 Логические выражения для чисел.

--- Z_1_8.pas
+++ Z_1_8.pas
@@ -11,5 +11,20 @@ begin
   write('Введите положительное целое число: ');
   readln(third);
   
+  if (first = second) and (first = third) and (second = third)
+    then equally := true;
+    
+  if (first <> second) and (first <> third) and (second <> third)
+    then different := true;
   
+  if (first = second) or (first = third) or (second = third)
+    then two := true;
+  
+  if (first mod 2 <> 0) and (second mod 2 <> 0) and (third mod 2 <> 0)
+    then odd := true;
+    
+  writeln('Все числа совпадают  ', equally);
+  writeln('Все числа разные ', different);
+  writeln('Два из трёх чисел совпадают  ', two);
+  writeln('Все числа нечётные ', odd);
 end.
