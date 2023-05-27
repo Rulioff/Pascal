1bf85b001a5a150ca8cfc382742af91760020dbe
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-05-27 18:43:30 +0300

    Update Z_1_8.pas
    Ненужные переменные и условные операторы.

--- Z_1_8.pas
+++ Z_1_8.pas
@@ -1,6 +1,12 @@
 ﻿{Z_1_8 Логические выражения для чисел}
+{Вводятся три целых положительных числа, программа печатает TRUE или FALSE для
+следующих булевых выражений:
+· все числа совпадают;
+· все числа разные;
+· два из трёх чисел совпадают;
+· все числа нечётные.}
 var first, second, third : integer;
-  equally, different, two, odd : boolean;
+//  equally, different, two, odd : boolean;
 begin
   write('Введите положительное целое число: ');
   readln(first);
@@ -11,20 +17,16 @@ begin
   write('Введите положительное целое число: ');
   readln(third);
   
-  if (first = second) and (first = third) and (second = third)
-    then equally := true;
+// equally := (first = second) and (first = third) and (second = third);
+        
+// different := (first <> second) and (first <> third) and (second <> third);
     
-  if (first <> second) and (first <> third) and (second <> third)
-    then different := true;
+// two := (first = second) or (first = third) or (second = third);
   
-  if (first = second) or (first = third) or (second = third)
-    then two := true;
-  
-  if (first mod 2 <> 0) and (second mod 2 <> 0) and (third mod 2 <> 0)
-    then odd := true;
+// odd := (first mod 2 <> 0) and (second mod 2 <> 0) and (third mod 2 <> 0);
     
-  writeln('Все числа совпадают  ', equally);
-  writeln('Все числа разные ', different);
-  writeln('Два из трёх чисел совпадают  ', two);
-  writeln('Все числа нечётные ', odd);
+  writeln('Все числа совпадают  ', (first = second) and (first = third) and (second = third));
+  writeln('Все числа разные ', (first <> second) and (first <> third) and (second <> third));
+  writeln('Два из трёх чисел совпадают  ', (first = second) or (first = third) or (second = third));
+  writeln('Все числа нечётные ', (first mod 2 <> 0) and (second mod 2 <> 0) and (third mod 2 <> 0));
 end.
