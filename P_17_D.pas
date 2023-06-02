1335b05c86fba80e0525ab9d0be36802a685a85e
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-02 21:02:33 +0300

    Create P_17_D.pas
    P_17_D Умножение суммированием.

---
+++ P_17_D.pas
@@ -0,0 +1,23 @@
+﻿{P_17_D Умножение суммированием.}
+{Пусть программа запросит два числа N и M, а затем вычислит их
+произведение без использования операции умножения (*). Подсказка: организуйте
+цикл суммирования N раз числа M.}
+
+var
+  number, number2, amount : integer;
+  begin
+    write('Введите первый множитель: ');
+    readln(number);
+    
+    write('Введите второй множитель: ');
+    readln(number2);
+    amount := number;
+    
+    repeat
+      amount := amount + number;
+      dec(number2);
+    until number2 <= 1;
+    
+    writeln('Произведение = ', amount);
+    
+  end.
