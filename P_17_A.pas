5bafbf530a162df61cd1f5ff6be2a315e31d27e0
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-04 14:20:53 +0300

    Update P_17_A.pas
    P_17_A Отказ от сдачи экзамена при вводе 0 бе вывода оценки.

--- P_17_A.pas
+++ P_17_A.pas
@@ -3,16 +3,17 @@
 ввод нуля в качестве ответа. В этом случае надо досрочно выйти из цикла и обойти
 выставляющий оценку оператор.}
 var
-  number, numberTwo, amount : integer; { сомножители и произведение }
-  count, error : integer; { счетчик вопросов и счетчик ошибок }
+  number, numberTwo, amount : integer; // Сомножители и произведение.
+  count, error : integer; // Счётчик вопросов и счетчик ошибок.
   score: string;
+  
 begin
   randomize;
-  error := 0; { обнуляем счетчики ошибок }
+  error := 0; // Обнуляем счетчики ошибок.
   count := 1;
-  writeln('Для отмены экзамена нажмите 0 пожалуйста.');
+  writeln('Для отмены экзамена введит 0 пожалуйста.');
+  
   repeat
-
     number := 1 + random(10);
     numberTwo := 1 + random(10);
     write(count, ' Сколько будет ', number,' * ', numberTwo, ' ? ');
@@ -21,18 +22,18 @@ begin
       then break;
     inc(count);  
   
-    { Если ответ неверный, увеличиваем счетчик ошибок }
+    // Если ответ неверный, увеличиваем счетчик ошибок.
     if (number * numberTwo <> amount) and (amount <> 0)
       then inc(error);
     
-    case error of { выставляем оценку }
-      0 : score := 'Отлично!';
-      1,2 : score := 'Хорошо';
-      3..5 : score := 'Удовлетворительно';
-    else score := 'Неуд!';    
-    end;
-    
-  until count > 15; //15 вопросов. 
-writeln(score, ' Нажмите Enter');
-readln;
+    case error of // Выставляем оценку.
+      0 : score := 'отлично!';
+      1,2 : score := 'хорошо';
+      3..5 : score := 'удовлетворительно';
+    else score := 'неуд!';    
+    end;    
+  until count > 15; // 15 вопросов.
+  
+  if amount <> 0
+    then writeln('Ваша оценка ', score);
 end.
