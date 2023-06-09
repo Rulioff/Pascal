1112f00e60f2b3d4c2aff5e16cd79826a3220da8
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-09 20:05:20 +0300

    Create P_17_J.pas
    P_17_E Сумма последовательности с условием.

---
+++ P_17_J.pas
@@ -0,0 +1,17 @@
+﻿{P_17_E Сумма последовательности.}
+{Напишите программу, вычисляющую сумму чисел от 1 до N, где N —
+число, вводимое пользователем.}
+var
+  number, i, sum : integer;
+  begin
+    write('Введите количество чисел для суммы пожалуйста: ');
+    readln(number);
+    sum := 0;
+//    i := 1;
+    
+    for i := 1 to number do
+      sum := i + sum;
+    
+    writeln(number);
+    writeln('Сумма ', number, ' чисел = ', sum);
+  end.
