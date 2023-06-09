da2dd74463dbb5c961fcd050f4781ee2d7500fd9
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-09 21:13:52 +0300

    Update P_17_J.pas
    P_17_E Сумма последовательности с условием.

--- P_17_J.pas
+++ P_17_J.pas
@@ -2,16 +2,17 @@
 {Напишите программу, вычисляющую сумму только тех чисел от 1 до N,
 которые делятся либо на три, либо на пять.}
 var
-  number, i, sum : integer;
-  split : boolean;
+  number, i, split3, split5, sum : integer;
+
   begin
     write('Введите количество чисел для суммы пожалуйста: ');
     readln(number);
     sum := 0;
-    split := (i mod 3 = 0) or (i mod 5 = 0);
+    split3 := (i mod 3);
+    split5 := (i mod 5);
     
     for i := 1 to number do
-      if split
+      if (split3 = 0) or (split5 = 0)
         then
           sum := i + sum;
     
