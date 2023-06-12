561aec5da9b16f4bd2c4a32c526db247c1f1be66
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-12 16:08:16 +0300

    Update P_15_G.pas
    P_15_G Улучшение читаемости.

--- P_15_G.pas
+++ P_15_G.pas
@@ -1,10 +1,14 @@
 ﻿{P_15_G Ряд случайных булевых значений (False, True)}
-var i : integer;
+{Найдите способ сформировать ряд случайных булевых значений (False, True),
+напечатайте 20 из них.}
+
+var number : integer;
+
 begin
   randomize; // <--
-  i := 0;
+  number := 0;
   repeat
     writeln(random(2) = random(2));
-    i := i + 1;
-  until i = 20;
+    number := number + 1;
+  until number = 20;
 end.
