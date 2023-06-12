66709d834d64b90d84e44d528aae6f25c7ef5ef0
Алексей Рулёв <ruljova@gmail.com>
2023-06-12 11:40:32 +0300

    Create Z_1_16.pas
    Z_1_16 Обмен значений_2.

---
+++ Z_1_16.pas
@@ -0,0 +1,17 @@
+﻿{Z_1_16 Обмен значений_2.}
+{Обменять значения числовых переменных A и B, воспользовавшись третьей переменной.}
+var
+  numberA, numberB, : integer;
+  
+  begin
+    write('Введите А, пожалуйста: ');
+    readln(numberA);
+    
+    write('Введите B, пожалуйста: ');
+    readln(numberB);
+
+    numberA := numberB;
+    numberB := buffer;
+    
+    writeln('Обмен значений A = ', numberA, ' и B = ', numberB, '.');
+  end.
