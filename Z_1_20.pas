366bdfff4d14b096a114aa28df04b97d3906ed95
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-12 17:32:37 +0300

    Create Z_1_20.pas
    Z_1_20 Среднее из трёх

---
+++ Z_1_20.pas
@@ -0,0 +1,24 @@
+﻿{Z_1_20 Среднее из трёх.}
+{Пользователь вводит три числа, а программа печатает среднее из них.}
+var
+  number1, number2, number3, middle : integer;
+  
+  begin
+    write('Введите 1-е число, пожалуйста: ');
+    readln(number1);
+    
+    write('Введите 2-е число, пожалуйста: ');
+    readln(number2);
+    
+    write('Введите 3-е число, пожалуйста: ');
+    readln(number3);
+    
+    middle := ((number1 > number2) and (number1 < number3)) ? number1 : number2;
+    middle := ((number1 > number3) and (number1 < number2)) ? number1 : number3;
+    middle := ((number2 > number1) and (number2 < number3)) ? number2 : number3;
+    middle := ((number2 > number3) and (number2 < number1)) ? number2 : number1;
+    middle := ((number3 > number1) and (number3 < number2)) ? number3 : number1;
+    middle := ((number3 > number2) and (number3 < number1)) ? number3 : number2;
+    
+    writeln('Среднее из 3-х число: ', middle, '.');
+  end.
