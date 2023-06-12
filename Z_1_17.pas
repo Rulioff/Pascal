45f91e9e12ab448640ed770f5456600a56e4e58c
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-12 15:52:53 +0300

    Create Z_1_17.pas
    Z_1_17 Упорядочение чисел.

---
+++ Z_1_17.pas
@@ -0,0 +1,18 @@
+﻿{Z_1_17 Упорядочение чисел.}
+{Пользователь вводит два числа, а программа печатает сначала меньшее, затем большее.}
+var
+  numberA, numberB : integer;
+  
+  begin
+    write('Введите А, пожалуйста: ');
+    readln(numberA);
+    
+    write('Введите B, пожалуйста: ');
+    readln(numberB);
+    
+    numberA := (numberA > numberB) ? numberA + numberB : numberA;
+    numberB := (numberA > numberB) ? numberA - numberB : numberB;
+    numberA := (numberA > numberB) ? numberA - numberB : numberA;
+    
+    writeln('Обмен значений A ', numberA, ' < B ', numberB, '.');
+  end.
