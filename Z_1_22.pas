68b985a32da8007f5794e3fcb33e38e013e5d224
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-13 21:17:25 +0300

    Update Z_1_22.pas
    Z_1_22 Два ближайших числа.

--- Z_1_22.pas
+++ Z_1_22.pas
@@ -2,7 +2,7 @@
 {Программа принимает три числа и печатает пару тех из них, что меньше отличаются друг
 от друга.}
 var
-  number1, number2, number3, difference1, difference2 : integer;
+  number1, number2, number3, difference1, difference2, buffer : integer;
   
   begin
     write('Введите 1-е число, пожалуйста: ');
@@ -13,10 +13,31 @@ var
     
     write('Введите 3-е число, пожалуйста: ');
     readln(number3);
+
+    if number1 > number2 then begin
+      buffer := number1;
+      number1 := number2;
+      number2 := buffer;
+    end;
     
-    if (number1 < number2) and (number2 < number3)
+    if number2 > number3 then begin
+      buffer := number2;
+      number2 := number3;
+      number3 := buffer;
+    end;
+    
+    if number1 > number3 then begin
+      buffer := number1;
+      number1 := number3;
+      number3 := buffer;
+    end;
+          
+    difference1 := number2 - number1;
+    difference2 := number3 - number2;
+    
+    if difference1 < difference2
       then
-        difference1 := number2 - number1;
-   
-    writeln('Меньше отличается друг от друга числа: ', difference1, ' и ', difference2, '.');
+        writeln('Меньше отличается друг от друга числа: ', number1, ' и ', number2, '.')
+      else
+        writeln('Меньше отличается друг от друга числа: ', number2, ' и ', number3, '.');      
   end.
