d2e012eb1995b608a9f9ac02e1640ca8f63b7789
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-18 17:34:37 +0300

    Update Z_1_35.pas
    Z_1_35 Наибольший общий делитель (НОД).

--- Z_1_35.pas
+++ Z_1_35.pas
@@ -1,12 +1,30 @@
 ﻿{Z_1_35 Наибольший общий делитель (НОД).}
 {Пусть даны два целых числа N и M, надо найти их наибольший общий делитель.}
 var
-  numberN, numberM, gcd : integer;
-  begin
+  numberN, numberM, buffer, gcd: integer;
+
+begin
   write('Введите число N, пожалуйста: ');
   readln(numberN);
   
   write('Введите число M, пожалуйста: ');
   readln(numberM);
   
-  end.
+  if numberN > numberM then begin
+    buffer := numberN;
+    numberN := numberM;
+    numberM := buffer;
+  end;
+  
+  //writeln(numberN, ' ', numberM);
+  
+  while numberM <> numberN do
+  begin
+      gcd := numberM - numberN;
+      numberM := numberN;
+      numberN := gcd;
+  //    writeln(numberN, ' ', numberM, ' ', gcd);
+  end;
+  
+  writeln('Наибольший общий делитель = ', gcd);
+end.
