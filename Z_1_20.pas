c4aa58208a6777ab4b9500abe52ae9eb31547311
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-18 14:25:33 +0300

    Update Z_1_20.pas
    Полная переделка.

--- Z_1_20.pas
+++ Z_1_20.pas
@@ -1,24 +1,30 @@
 ﻿{Z_1_20 Среднее из трёх.}
 {Пользователь вводит три числа, а программа печатает среднее из них.}
 var
-  number1, number2, number3, middle : integer;
+  number1, number2, number3, middle: integer;
+
+begin
+  write('Введите 1-е число, пожалуйста: ');
+  readln(number1);
   
-  begin
-    write('Введите 1-е число, пожалуйста: ');
-    readln(number1);
-    
-    write('Введите 2-е число, пожалуйста: ');
-    readln(number2);
-    
-    write('Введите 3-е число, пожалуйста: ');
-    readln(number3);
-    
-    middle := ((number1 > number2) and (number1 < number3)) ? number1 : number2;
-    middle := ((number1 > number3) and (number1 < number2)) ? number1 : number3;
-    middle := ((number2 > number1) and (number2 < number3)) ? number2 : number3;
-    middle := ((number2 > number3) and (number2 < number1)) ? number2 : number1;
-    middle := ((number3 > number1) and (number3 < number2)) ? number3 : number1;
-    middle := ((number3 > number2) and (number3 < number1)) ? number3 : number2;
-    
-    writeln('Среднее из 3-х число: ', middle, '.');
-  end.
+  write('Введите 2-е число, пожалуйста: ');
+  readln(number2);
+  
+  write('Введите 3-е число, пожалуйста: ');
+  readln(number3);
+  
+  middle := 0;
+  
+    // 123, 231, 312, 132, 321, 213
+  
+  if ((number1 <= number2) and (number2 <= number3)) then middle := number2; //123
+  if ((number1 >= number2) and (number2 >= number3)) then middle := number2; //321
+  
+  if ((number1 <= number2) and (number2 >= number3)) then middle := number1; //231
+  if ((number1 >= number2) and (number2 <= number3)) then middle := number1; //213
+  
+  if ((number1 >= number2) and (number2 <= number3)) then middle := number3; //312
+  if ((number1 <= number2) and (number2 >= number3)) then middle := number3; //132
+  
+  writeln('Среднее из 3-х число: ', middle, '.');
+end.
