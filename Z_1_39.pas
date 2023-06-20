e6906fbe8544ff47c6f1ff5af9b024291acb51cc
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-20 19:18:35 +0300

    Update Z_1_39.pas
    Z_1_39 Проверка на Фибоначчи. Решена.

--- Z_1_39.pas
+++ Z_1_39.pas
@@ -2,15 +2,12 @@
 {Дано число, программа должна напечатать «ДА», если оно принадлежит ряду Фибоначчи,
 и «НЕТ» в противном случае.}
 var
-  number, numberE, numberF, numberN, buffer: integer;
+  number, numberE, numberF, buffer: integer;
   hasFibonacci: boolean;
 
 begin
   write('Введите число - от 1 до ∞, пожалуйста: ');
   readln(numberE);
-  
-//  randomize();
-//  numberN := 1 + random(99999);
   
   number := 1;
   buffer := 0;
@@ -18,22 +15,22 @@ begin
   numberF := 0;
   
   repeat
-   begin
+    begin
       numberF := number + buffer;
       number := buffer;
       buffer := numberF;
-   //   numberN := numberN + 1;
     end;
+    
     if numberF = numberE then begin
       hasFibonacci := true;
       break;
     end;
-  until numberE = numberF = false;
+  until numberE < numberF;
   
-  writeln(numberE, ' ', numberF, ' ', hasFibonacci,' ', numberN);
+  //Точка отладки. writeln(numberE, ' ', numberF, ' ', hasFibonacci);
   
   if hasFibonacci = true then
-  writeln('Число ', numberE, ' принадлежит ряду Фибоначчи? ', 'Да.')
+    writeln('Число ', numberE, ' принадлежит ряду Фибоначчи? ', 'Да.')
   else
-  writeln('Число ', numberE, ' принадлежит ряду Фибоначчи? ', 'Нет.') 
+    writeln('Число ', numberE, ' принадлежит ряду Фибоначчи? ', 'Нет.')
 end.
