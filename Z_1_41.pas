98f7291d30244fd8383bc65f1ccf2b74d4389983
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-06-25 15:42:40 +0300

    Update Z_1_41.pas
    Z_1_41 Печать простых чисел. Доработать 1-й пример.

--- Z_1_41.pas
+++ Z_1_41.pas
@@ -4,8 +4,9 @@
 1) Когда известно, что A < B
 2) Когда соотношение между A и B неизвестно.}
 var
-  numberA, numberB, ask, number, numberN, count: integer;
-  
+  numberA, numberB, number, numberN, count, numberPrime, buffer: integer;
+  ask: string;
+
 begin
   repeat
     write('Введите целое положительное число A, пожалуйста: ');
@@ -13,28 +14,45 @@ begin
     
     write('Введите целое положительное число B, пожалуйста: ');
     readln(numberB);
-  until (numberA > 0) and (numberB >0);
-  
-  write('Известно, что A < B? Введите да или нет, пожалуйста: ');
-  readln(ask);
-  
-  numberN := 0;
-  count := 0;
+  until (numberA > 0) and (numberB > 0);
   
   repeat
-    begin        
-    numberN += 1;
-    if number mod numberN = 0 then
-      count += 1;
- 
-    if count > 2 then break;
+    write('Известно, что A < B? Введите да или нет, пожалуйста: ');
+    readln(ask);
     
-    writeln(number, ' ', numberN, ' ', count);
-    end;
-  until number <= numberN;
+    if ask = 'да' then break;
+    
+    if ask = 'нет' then
+      if numberA > numberB then begin
+        buffer := numberA;
+        numberA := numberB;
+        numberB := buffer;
+      end;
+  until (ask = 'да') or (ask = 'нет');
   
-  if count <= 2 then
-    writeln('Число ', number, ' проcтое? ', 'Да.');
-  if count > 2 then
-    writeln('Число ', number, ' проcтое? ', 'Нет.');
+  for number := numberA to numberB do
+  begin
+    numberN := 0;
+    count := 0;
+    numberPrime := 0;
+    
+    repeat
+      begin
+        numberN += 1;
+        if number mod numberN = 0 then
+          count += 1;
+        
+        if count > 2 then break;
+        
+        //    writeln(number, ' ', numberN, ' ', count);
+      end;
+    until number <= numberN;
+    
+    if count <= 2 then begin
+      numberPrime := number;    
+      write('Число ', numberPrime, ' проcтое. ');
+    end;
+  end;
+  if numberPrime = 0 then
+    writeln(' Пусто.');
 end.
