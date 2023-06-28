aa02a1afa6e6230d565f80c9399c2ec0926b98d1
Алексей Рулёв <ruljova@gmail.com>
2023-06-28 13:22:44 +0300

    Update Z_1_40.pas
    Z_1_40 Проверка числа на простоту. Исправил нейминг. Решена.

--- Z_1_40.pas
+++ Z_1_40.pas
@@ -1,33 +1,32 @@
-﻿{Z_1_40 Проверка числа на простоту. Доработать нейминг.}
+﻿{Z_1_40 Проверка числа на простоту.}
 {Ввести целое положительное число и напечатать «ДА», если это простое число и «НЕТ» в
 противном случае. Простым называется число, делящееся без остатка лишь на 1 и само на
 себя.}
 var
-  number, numberN, count: integer;
+  inputNumber, indexNumber, primeNumber: integer;
 
 begin
   repeat
     write('Введите целое положительное число, пожалуйста: ');
-    readln(number);
-  until number > 0;
+    readln(inputNumber);
+  until inputNumber > 0;
   
-  numberN := 0;
-  count := 0;
+  indexNumber := 0;
+  primeNumber := 0;
   
   repeat
     begin        
-    numberN += 1;
-    if number mod numberN = 0 then
-      count += 1;
+    indexNumber += 1;
+    if inputNumber mod indexNumber = 0 then
+      primeNumber += 1;
  
-    if count > 2 then break;
+    if primeNumber > 2 then break;
     
-    writeln(number, ' ', numberN, ' ', count);
     end;
-  until number <= numberN;
+  until inputNumber <= indexNumber;
   
-  if count <= 2 then
-    writeln('Число ', number, ' проcтое? ', 'Да.');
-  if count > 2 then
-    writeln('Число ', number, ' проcтое? ', 'Нет.');
+  if primeNumber <= 2 then
+    writeln('Число ', inputNumber, ' проcтое? ', 'Да.')
+  else
+    writeln('Число ', inputNumber, ' проcтое? ', 'Нет.');
 end.
