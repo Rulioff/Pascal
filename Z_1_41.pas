3b1498c3abfffd348db3a154a129dba84473d22b
Алексей Рулёв <ruljova@gmail.com>
2023-06-30 14:37:15 +0300

    Update Z_1_41.pas
    Z_1_41 Печать простых чисел. Нужно убрать "Пусто" после простых чисел.

--- Z_1_41.pas
+++ Z_1_41.pas
@@ -5,7 +5,7 @@
 2) Когда соотношение между A и B неизвестно.}
 var
   firstNumber, lastNumber, currentNumber, divisor, shift: integer;
-  empty, primeNumber: boolean;
+  empty: boolean;
 
 begin
   repeat
@@ -22,27 +22,25 @@ begin
     firstNumber := lastNumber;
     lastNumber := shift;
   end;
-  empty := true;
   
   for currentNumber := firstNumber to lastNumber do
   begin
     divisor := 0;
-    primeNumber := true;
+    empty := false;
     
     repeat
       begin
         divisor += 1;
         if (currentNumber mod divisor = 0) and (divisor <> 1) and (currentNumber <> divisor) then
-          primeNumber := false;
-        
-        if primeNumber = false then break;        
+        begin
+          empty := true;
+          break;
+        end;       
       end;
     until currentNumber <= divisor;
     
-    if primeNumber then begin
-      empty := false;  
+    if empty = false then
       write('Число ', currentNumber, ' проcтое. ');
-    end;
   end;
   
   if empty then
