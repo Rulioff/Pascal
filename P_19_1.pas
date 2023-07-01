5c8dafec52af477bf22650a6f76836288ce42101
Алексей Рулёв <ruljova@gmail.com>
2023-07-01 12:35:08 +0300

    Create P_19_1.pas
    P_19_1 Пример применения процедуры. Решена из теории. Поправил нейминг.

---
+++ P_19_1.pas
@@ -0,0 +1,16 @@
+﻿{ P_19_1 Пример применения процедуры.}
+var
+  man: string;
+
+procedure pause; //--- описание процедуры ---
+begin
+  write('Нажмите Enter…');
+  readln;
+end;
+
+begin //--- главная программа ---
+  writeln('Как тебя зовут?');
+  readln(Man);
+  writeln('Здравствуй, ', man);
+  pause; //Вызов процедуры.
+end.
