59ff9fe753134a295f5f26e6fdcef19236eef9ee
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-07-02 19:05:13 +0300

    Z_1_41 Печать простых чисел. Отформатировал по стандарту FreePascal.

--- Z_1_41.pas
+++ Z_1_41.pas
@@ -3,46 +3,53 @@
 на этом интервале (включая крайние). Сделать два варианта программы:
 1) Когда известно, что A < B
 2) Когда соотношение между A и B неизвестно.}
+program Z_1_41;
+
 var
   firstNumber, lastNumber, currentNumber, divisor, shift: integer;
   isEmpty, isPrimeNumber: boolean;
 
 begin
   repeat
-    write('Введите целое положительное число A, пожалуйста: ');
+    Write('Введите целое положительное число A, пожалуйста: ');
     readln(firstNumber);
-    
-    write('Введите целое положительное число B, пожалуйста: ');
-    readln(lastNumber);
+
+    Write('Введите целое положительное число B, пожалуйста: ');
+    ReadLn(lastNumber);
   until (firstNumber > 0) and (lastNumber > 0);
-  
+
   // Для 1-го варианта удалите строки 20 ... 24.
-  if firstNumber > lastNumber then begin
+  if firstNumber > lastNumber then
+  begin
     shift := firstNumber;
     firstNumber := lastNumber;
     lastNumber := shift;
   end;
-  isEmpty := true;
-  
+  isEmpty := True;
+
   for currentNumber := firstNumber to lastNumber do
   begin
     divisor := 0;
-    isPrimeNumber := true;
-    
+    isPrimeNumber := True;
+
     repeat
       divisor += 1;
-      if (currentNumber mod divisor = 0) and (divisor <> 1) and (currentNumber <> divisor) then begin
-        isPrimeNumber := false;
-        break;        
+      if (currentNumber mod divisor = 0) and (divisor <> 1) and
+        (currentNumber <> divisor) then
+      begin
+        isPrimeNumber := False;
+        break;
       end;
     until currentNumber <= divisor;
-    
-    if isPrimeNumber then begin
-      isEmpty := false;  
-      write('Число ', currentNumber, ' проcтое. ');
+
+    if isPrimeNumber then
+    begin
+      isEmpty := False;
+      Write('Число ', currentNumber, ' проcтое. ');
     end;
   end;
-  
-  if isEmpty then
-    writeln('Пусто.');
+
+  if isEmpty then writeln('Пусто.');
+
+  ReadLn;
 end.
