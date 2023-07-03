c5f2af5ed216a0b31fe9e80ed0db6ac435ea7b32
Алексей Рулёв <ruljova@gmail.com>
2023-07-03 14:23:19 +0300

    Create P_19_2.pas
    P_19_2 Сделали рефакторинг. Привести к freePascale

---
+++ P_19_2.pas
@@ -0,0 +1,18 @@
+﻿{P_19_2 Применение процедуры с параметром.}
+var
+  man: string;
+
+procedure Pause (msg : string); //--- объявление процедуры с параметром msg ---
+begin
+  write(msg);
+  readln;
+end;
+
+begin //--- главная программа ---
+  writeln('Как тебя зовут?');
+  readln(Man);
+  writeln('Здравствуй, ', man);
+  Pause('Нажмите Enter…'); //Вызов процедуры.
+  Pause('Ещё раз…');
+  Pause('И ещё разок!');
+end.
