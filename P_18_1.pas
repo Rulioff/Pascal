9761b3ebdd3ff697bf8a6321a89364b82a80527e
Алексей Рулёв <ruljova@gmail.com>
2023-07-07 15:43:42 +0300

    P_18_1 Решена. Использовали рефакторинг

--- P_18_1.pas
+++ P_18_1.pas
@@ -1,14 +1,20 @@
-{ P_18_1 ╨а╨░╤Б╨┐╨╡╤З╨░╤В╨║╨░ ╨╛╤В╨┤╨╡╨╗╤М╨╜╤Л╤Е ╤Б╨╕╨╝╨▓╨╛╨╗╨╛╨▓ ╤Б╤В╤А╨╛╨║╨╕.}
-var S: string;
-symbol: char;
-k, L : integer;
+{ P_18_1 Распечатка отдельных символов строки.}
+program P_18_1;
+
+{$CODEPAGE CP866}
+var
+  line: string;
+  symbol: char;
+  index, symbolsCount: integer;
 begin
-repeat
-Write('╨Т╨▓╨╡╨┤╨╕╤В╨╡ ╤Б╤В╤А╨╛╨║╤Г: '); Readln(S);
-L:= Length(S); { ╨╛╨┐╤А╨╡╨┤╨╡╨╗╤П╨╡╨╝ ╨┤╨╗╨╕╨╜╤Г ╤Б╤В╤А╨╛╨║╨╕ }
-for k:=1 to L do begin
-symbol:= S[k]; { ╨▓╤Л╨▒╨╕╤А╨░╨╡╨╝ ╨╛╤З╨╡╤А╨╡╨┤╨╜╨╛╨╣ ╤Б╨╕╨╝╨▓╨╛╨╗ }
-Writeln(symbol); { ╨╕ ╨┐╨╡╤З╨░╤В╨░╨╡╨╝ ╨╡╨│╨╛ ╨▓ ╨╛╤В╨┤╨╡╨╗╤М╨╜╨╛╨╣ ╤Б╤В╤А╨╛╨║╨╡ }
-end;
-until L=0; { L=0, ╨╡╤Б╨╗╨╕ ╤Б╤В╤А╨╛╨║╨░ ╨┐╤Г╤Б╤В╨░ }
+  repeat
+    Write('Введите строку, пожалуйста: ');
+    Readln(line);
+    symbolsCount := Length(line); { определяем длину строки }
+    for index := 1 to symbolsCount do
+    begin
+      symbol := line[index]; { выбираем очередной символ }
+      Writeln(symbol); { и печатаем его в отдельной строке }
+    end;
+  until symbolsCount = 0; { symbolsCount=0, если строка пуста }
 end.
