9ef788732982d50dcc73d0f7df102eefe825f86e
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-07-19 21:36:59 +0300

    P_18_G Проверить алгоритм, не убирает лишние символы

--- P_18_G.pas
+++ P_18_G.pas
@@ -1,23 +1,30 @@
-{ P_18_G Пусть ваша программа удалит из такой строки все
-символы, кроме цифр. Например, (8)123-45-67 после ввода указанного выше номера она должна
+{ P_18_G Пусть ваша программа удалит из такой строки все символы, кроме цифр.
+Например, (8)123-45-67 после ввода указанного выше номера она должна
 напечатать: 8123456789. }
 
 program P_18_G;
 
 {$mode objfpc}{$H+}
 {$codepage UTF8}
+
 var
   Line, PhoneNumber: string;
   Index: integer;
+  Symbol: char;
 
 begin
-  repeat
-    Write('Введите номер телефона, пожалуйста: ');
-    Readln(Line);
-  until Line;
+  Write('Введите номер телефона, пожалуйста: ');
+  Readln(Line);
+  PhoneNumber := '';
 
   for Index := 1 to Length(Line) do
-    Line := Line + Line[Index];
+  begin
+    Symbol := Line[Index];
+    if (Symbol = '0') or (Symbol = '1') or (Symbol = '2') or (Symbol = '3') or
+      (Symbol = '4') or (Symbol = '5') or (Symbol = '5') or (Symbol = '6') or
+      (Symbol = '7') or (Symbol = '8') or (Symbol = '9') then
+      PhoneNumber := PhoneNumber + Symbol;
+  end;
 
   Writeln('Номер сохранён: ', PhoneNumber);
   Readln;
