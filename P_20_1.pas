375a01958ee6e7aada828ad77cb0a0d73e82362b
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-07-23 16:08:38 +0300

    P_20_1 Создали главную программу, 1-й этап

---
+++ P_20_1.pas
@@ -0,0 +1,19 @@
+{ P_20_1 – первый этап разработки }
+
+program P_20_1;
+
+{$mode objfpc}{$H+}
+{$codepage UTF8}
+
+var
+  S: string;
+  k: integer;
+begin {--- главная программа ---}
+  for k := 1 to 3 do
+  begin
+    Write('Введите строку: ');
+    Readln(S);
+    { Scan(S); }
+    Writeln(S);
+  end;
+end.
