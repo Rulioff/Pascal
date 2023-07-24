5c2e4f64d516ee99c56982dfc9efa6194390d646
Алексей Рулёв <ruljova@gmail.com>
2023-07-24 15:25:23 +0300

    P_20_A Сделали рефакторинг и добавили процедуру Rating

--- P_20_A.pas
+++ P_20_A.pas
@@ -7,7 +7,39 @@ program P_20_A;
 {$mode objfpc}{$H+}
 {$codepage UTF8}
 
+var
+  Number, NumberTwo, Amount: integer;
+  { сомножители и произведение }
+  Count, Error: integer;
+  { счетчик вопросов и счетчик ошибок }
+  Score: string;
+
+  procedure Rating(Error: integer);
+  begin
+    case Error of { выставляем оценку }
+      0: Score := 'Отлично!';
+      1, 2: Score := 'Хорошо';
+      3..5: Score := 'Удовлетворительно';
+      else
+        Score := 'Ну оччччень плохо!';
+    end;
+  end;
 
 begin
-end.
+  randomize;
+  Error := 0; { обнуляем счетчики ошибок }
 
+  for Count := 1 to 15 do
+  begin { 15 вопросов }
+    Number := 1 + random(10);
+    NumberTwo := 1 + random(10);
+    Write(Count, ' Сколько будет ', Number, ' * ', NumberTwo, ' ? ');
+    readln(Amount);
+
+    { Если ответ неверный, увеличиваем счетчик ошибок }
+    if Number * NumberTwo <> Amount then Inc(Error);
+  end; { цикл и блок завершаются здесь}
+
+  writeln(Rating, ' Нажмите Enter');
+  readln;
+end.
