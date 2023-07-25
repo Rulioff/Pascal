db8cf6be0ae32fda9534975e82e7f3bd611e5e21
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-07-25 21:20:26 +0300

    P_20_B Создали главную программу и заглушку процедуры

---
+++ P_20_B.pas
@@ -0,0 +1,24 @@
+{Создайте процедуру, печатающую все числа, кроме единицы, на которые
+без остатка делится число N, где N — параметр процедуры. Напишите программу
+для проверки этой процедуры.}
+
+program P_20_B;
+
+{$mode objfpc}{$H+}
+{$codepage UTF8}
+var
+  Divisible, Divider, Quotient, Index: integer;
+
+  procedure Multiplicity(Divisible: integer);
+  begin
+repeat
+until
+  end;
+
+begin
+  Write('Введите число для определения кратности: ');
+  ReadLn(Divisible);
+  Multiplicity(Divisible);
+  Quotient:= Divisible / Divisible;
+  WriteLn(Divisible, '/', Divider, '=', Quotient);
+end.
