da7da8064c93f7d9487e562fe0eaa27ca13f1a4d
Алексей Рулёв <ruljova@gmail.com>
2023-08-03 17:23:31 +0300

    P_22_A Скопировали и проверили работу программы P_22_1

---
+++ P_22_A.pas
@@ -0,0 +1,30 @@
+{ P_22_A – процедура обмена и программа её проверки }
+
+program P_22_A;
+
+{$mode objfpc}{$H+}
+{$codepage UTF8}
+
+  // процедура обмена
+  procedure Swap(var First, Second: integer);
+  var
+    Buffer: integer;
+
+  begin
+    Buffer := First;
+    First := Second;
+    Second := Buffer;
+  end;
+
+var
+  NumberOne, NumberTwo: integer;
+
+  //--- главная программа ---
+begin
+  NumberOne := 10;
+  NumberTwo := 20;
+  Writeln('A= ', NumberOne, ' B= ', NumberTwo);
+  Swap(NumberOne, NumberTwo);
+  Writeln('A= ', NumberOne, ' B= ', NumberTwo);
+  Readln;
+end.
