b5e90709df80f62f2246fd940df612601e5be4b6
Алексей Рулёв <ruljova@gmail.com>
2023-08-04 18:37:43 +0300

    P_22_V Исправили переменные, работает только с латиницей. Проверить переменную Res

--- P_22_V.pas
+++ P_22_V.pas
@@ -7,26 +7,26 @@ program P_22_V;
 {$codepage UTF8}
 
 var
-  Line: string;
+  Line: string; { глобальная переменная }
   Symbol: char;
-  Index, Result: integer; { глобальная переменная }
+  Res: integer;
 
-  procedure Calc(const LineSent: string; Symbol: char; var Res: integer);
+  procedure Calc(const LineSent: string; var Symbol: char; var Res: integer);
   var
     Index: integer;
   begin
     Res := 0;
-    for Index := 1 to Length(Line) do
-      if Line[Index] = Symbol then
+    for Index := 1 to Length(LineSent) do
+      if LineSent[Index] = Symbol then
         Res := Res + 1;
   end;
 
 begin { главная программа }
   Write('Введите строку, пожалуйста: ');
   Readln(Line);
-  Write('Введите символ, который будем менять, пожалуйста: ');
+  Write('Введите символ, чтобы узнать его количество в строке, пожалуйста: ');
   Readln(Symbol);
-  Calc(Line, 'a', Result);
-  Writeln('Длина строки: ', Result, ' символов.');
+  Calc(Line, Symbol, Res);
+  Writeln('Строка ', Line, ' содержит ', Res, ' ', Symbol);
   ReadLn;
 end.
