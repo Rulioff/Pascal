63bfb477940e6242618ed9300abb44a09aa84925
Алексей Рулёв <ruljova@gmail.com>
2023-08-04 13:50:14 +0300

    P_22_V Добавили запятую в 28 стр, отформатировали код. проверить лишние переменные

--- P_22_V.pas
+++ P_22_V.pas
@@ -9,22 +9,24 @@ program P_22_V;
 var
   Line: string;
   Symbol: char;
-  Index, Res: integer; { глобальная переменная }
+  Index, Result: integer; { глобальная переменная }
 
-  procedure Calc(LineSent: string; Symbol: char; var Res: integer);
+  procedure Calc(const LineSent: string; Symbol: char; var Res: integer);
   var
     Index: integer;
   begin
-    Res:= 0;
+    Res := 0;
     for Index := 1 to Length(Line) do
       if Line[Index] = Symbol then
-      Res:= Res + 1;
+        Res := Res + 1;
   end;
 
 begin { главная программа }
   Write('Введите строку, пожалуйста: ');
   Readln(Line);
-  Calc(Line, Res);
-  Writeln('Длина строки: 'Res, ' символов.');
+  Write('Введите символ, который будем менять, пожалуйста: ');
+  Readln(Symbol);
+  Calc(Line, 'a', Result);
+  Writeln('Длина строки: ', Result, ' символов.');
   ReadLn;
 end.
