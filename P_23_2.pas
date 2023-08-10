485ae50a2c77ec2cae0cf8118f83ca228421a243
Алексей Рулёв <ruljova@gmail.com>
2023-08-10 16:10:22 +0300

    P_23_2 Дорабатали P_20_1, заменили процедуру на функцию. Компилятор предлагает Scan заменить на Result

---
+++ P_23_2.pas
@@ -0,0 +1,30 @@
+{ P_23_2 Вернемся к программе P_20_1, замена символов в строке с применением функции }
+program P_23_2;
+
+{$mode objfpc}{$H+}
+{$codepage UTF8}
+
+var
+  Line: string;
+  Index: integer; { глобальная переменная }
+
+  function Scan(LineSent: string): string;
+  var
+    Index: integer; { локальная переменная }
+  begin
+    for Index := 1 to Length(LineSent) do
+      if LineSent[Index] = 'A' then
+        LineSent[Index] := 'B'; { замена в параметре LineSent }
+    Scan := LineSent;
+  end;
+
+begin { главная программа }
+  for Index := 1 to 3 do
+  begin
+    Write('Введите строку, пожалуйста: ');
+    Readln(Line);
+    Scan(Line);
+    Writeln(Scan(Line));
+    ReadLn;
+  end;
+end.
