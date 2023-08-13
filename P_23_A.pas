be1d732622827a476beff7a7e518db7098b161f1
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-08-13 16:06:55 +0300

    P_23_A Создали функцию поиска символа и программу. Добавить информативности на вывод

---
+++ P_23_A.pas
@@ -0,0 +1,27 @@
+{ P_23_A Напишите функцию для поиска буквы в заданной строке. Она должна
+возвращать TRUE, если в строке есть хоть одна эта буква, и FALSE в противном
+случае. Напишите программу для проверки функции. }
+program P_23_A;
+
+{$mode objfpc}{$H+}
+{$codepage UTF8}
+
+var
+  Line: string; { глобальная переменная }
+
+  function FindSymbol(SearchEngine: string): boolean;
+  var
+    Index: integer; { локальная переменная }
+  begin
+    for Index := 1 to Length(SearchEngine) do
+      if SearchEngine[Index] = 'A' then
+        Result := True;
+  end;
+
+begin
+  Write('Введите строку, пожалуйста: ');
+  Readln(Line);
+  FindSymbol(Line);
+  Writeln(FindSymbol(Line));
+  ReadLn;
+end.
