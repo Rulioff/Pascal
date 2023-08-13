bdc28024004d76c0c9db5ff2e8846c7ec6084d07
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-08-13 16:42:10 +0300

    Добавили в gitignore расширение *.ico

---
+++ p_23_b.pas
@@ -0,0 +1,29 @@
+{ P_23_B Напишите функцию для определения позиции буквы в заданной строке.
+Функция должна вернуть позицию первой такой буквы или ноль, если буквы в
+строке нет. Напишите программу для проверки функции }
+program P_23_B;
+
+{$mode objfpc}{$H+}
+{$codepage UTF8}
+
+var
+  Line: string; { глобальная переменная }
+
+  function PosSymbol(Position: integer): integer;
+  var
+    Index: integer; { локальная переменная }
+  begin
+    Result := 0;
+    for Index := 1 to Length(Position) do
+      if Position[Index] = 'A' then
+        Result := Index;
+  end;
+
+begin
+  Write('Введите строку, пожалуйста: ');
+  Readln(Line);
+  FindSymbol(Line);
+  Writeln('Символ - A находится на: ', FindSymbol(Line),
+    ' позиции в строке.');
+  ReadLn;
+end.
