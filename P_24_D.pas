8622b072f7ff981e5fe34d6c6a04aa4875ec121c
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-09-24 16:46:50 +0300

    P_24_D fix: Для задачи P_24_A функции EncryptChar и DecryptChar, чтобы они не изменяли управляющие символы

---
+++ P_24_D.pas
@@ -0,0 +1,81 @@
+{ P_24_D Строки текстовых файлов порой содержат управляющие символы,
+например символ горизонтальной табуляции (код 9). Обработка этих символов
+нашей программой нарушит структуру файла. Исправьте функции Encrypt и
+Decrypt так, чтобы они не изменяли символы, коды которых меньше 32.}
+program P_24_D;
+
+{$mode objfpc}{$H+}
+{$codepage UTF8}
+uses
+  SysUtils;
+
+{––––– Зашифровка одного символа –––––}
+  function EncryptChar(Symbol: char; Key: integer): char;
+  var
+    PosChar: integer;
+  begin
+    if Ord(Symbol) < 33 then
+      EncryptChar := Symbol;
+
+    PosChar := Ord(Symbol) + Key;
+    if PosChar > 255 then PosChar := PosChar - 256 + 32;
+    EncryptChar := Chr(PosChar);
+  end;
+
+  {––––– Расшифровка одного символа –––––}
+  function DecryptChar(Symbol: char; Key: integer): char;
+  var
+    PosChar: integer;
+  begin
+    if Ord(Symbol) < 33 then
+      DecryptChar := Symbol;
+
+    PosChar := Ord(Symbol) - Key;
+    if PosChar < 32 then PosChar := PosChar + 256 - 32;
+    DecryptChar := Chr(PosChar);
+  end;
+
+  {––––– Зашифровка строки –––––}
+  procedure EncryptStr(var Symbol: string; Key: integer);
+  var
+    CountSymbol: integer;
+  begin
+    for CountSymbol := 1 to Length(Symbol) do
+      Symbol[CountSymbol] := EncryptChar(Symbol[CountSymbol], Key);
+  end;
+
+  {––––– Расшифровка строки –––––}
+  procedure DecryptStr(var Symbol: string; Key: integer);
+  var
+    CountSymbol: integer;
+  begin
+    for CountSymbol := 1 to Length(Symbol) do
+      Symbol[CountSymbol] := DecryptChar(Symbol[CountSymbol], Key);
+  end;
+
+  {––––– Главная программа –––––}
+var
+  Line: string;
+  Key: integer;
+  Operation: integer;
+begin
+  repeat
+    Write('Введите ключ Цезаря от 1 до 25: ');
+    Readln(Key);
+
+    Write('Введите строку: ');
+    Readln(Line);
+
+    Writeln('Укажите операцию: 1– шифровать,' +
+      ' 2– расшифровать,' + ' Прочие – выход');
+    Readln(Operation);
+
+    case Operation of
+      1: EncryptStr(Line, Key);
+      2: DecryptStr(Line, Key);
+      else
+        Break;
+    end;
+    Writeln(Line); { печатаем результат }
+  until False;
+end.
