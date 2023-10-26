a40f0abca8c7b4b92c7b5d25658efbbab58f61f2
Алексей Рулёв <74359412+Rulioff@users.noreply.github.com>
2023-10-26 22:23:28 +0300

    P_26_V feat: Программа копирует содержимое файла в другой с перестановкой четных и нечетных строк и второй вариант решения с перестановкой строк в обратном порядке

--- P_26_V_1.pas
+++ P_26_V_1.pas
@@ -1,6 +1,6 @@
-{P_26_V_2 Скопировать один файл в другой с перестановкой строк в обратном порядке (см. условие задачи «Е» к 25-й
-главе).}
-program P_26_V_2;
+{P_26_V_1 Скопировать один файл в другой с перестановкой местами четных и нечетных строк.}
+
+program P_26_V_1;
 
 {$mode objfpc} {$codepage UTF-8}{$H+}
 
@@ -9,7 +9,7 @@ uses
 
 var
   SourceTextFile, TargetTextFile: Text;
-  SourceLineNumber, TargetLineNumber: integer;
+  SourceLineNumber, CurentLineNumber: integer;
 
   function GetFileLinesNumber(var TextFile: Text): integer;
   var
@@ -41,16 +41,25 @@ var
 
 begin
   Assign(SourceTextFile, 'Source_Text_26_V.txt');
-  Assign(TargetTextFile, 'Target_Text_26_V_2.txt');
+  Assign(TargetTextFile, 'Target_Text_26_V_1.txt');
   Rewrite(TargetTextFile);
   SourceLineNumber := GetFileLinesNumber(SourceTextFile);
-  for TargetLineNumber := SourceLineNumber downto 1 do
+  CurentLineNumber := 1;
+
+  while CurentLineNumber <= SourceLineNumber + (SourceLineNumber mod 2) do
   begin
-    if TargetLineNumber = 1 then
-      Write(TargetTextFile, GetFileLineByNumber(SourceTextFile, TargetLineNumber))
-    else
-      WriteLn(TargetTextFile, GetFileLineByNumber(SourceTextFile, TargetLineNumber));
+    if CurentLineNumber mod 2 <> 0 then
+    begin
+      WriteLn(TargetTextFile, GetFileLineByNumber(SourceTextFile,
+        CurentLineNumber + 1));
+      if CurentLineNumber = SourceLineNumber + (SourceLineNumber mod 2) - 1 then
+        Write(TargetTextFile, GetFileLineByNumber(SourceTextFile, CurentLineNumber))
+      else
+        WriteLn(TargetTextFile, GetFileLineByNumber(SourceTextFile, CurentLineNumber));
+      Inc(CurentLineNumber, 2);
+    end;
   end;
+
   Close(SourceTextFile);
   Close(TargetTextFile);
   ReadLn;
